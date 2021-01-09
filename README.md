5b09db8b376d138022e72f2c6b3ef3ca7d8426b8
최신우 <sinwoo1225@gmail.com>
2021-01-10 00:56:22 +0900

    우선순위큐 - 디스크 컨트롤러 문제풀이 커밋
    * 우선순위큐 - 디스크 컨트롤러 문제풀이 커밋

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 언어는 java입니다.
 
 ## 우선순위큐
-* [ ] 디스크 컨트롤러 (진행중) [링크](https://programmers.co.kr/learn/courses/30/lessons/42627)
+* [x] 디스크 컨트롤러  [문제링크](https://programmers.co.kr/learn/courses/30/lessons/42627) [풀이링크](https://github.com/sinwoo1225/programming-test-practice/blob/master/src/priorityqueue/DiskController.java)
--- src/priorityqueue/DiskController.java
+++ src/priorityqueue/DiskController.java
@@ -1,7 +1,52 @@
 package priorityqueue;
 
-public class DiskController {
-    public static void main(String[] args) {
+import java.util.*;
 
+public class DiskController {
+
+    public static int solution(int[][] jobs) {
+        List<int[]> jobList = new LinkedList<>(Arrays.asList(jobs));
+        int answer = 0;
+        int current_time = 0;
+
+        jobList.sort((arg0, arg1) -> {
+            if (arg0[0] == arg1[0]) {
+                if (arg0[1] == arg1[1]) {
+                    return 0;
+                } else if (arg0[1] < arg1[1]) {
+                    return -1;
+                } else {
+                    return 1;
+                }
+            } else if (arg0[0] < arg1[0]) {
+                return -1;
+            } else {
+                return 1;
+            }
+        });
+
+        while (jobList.size() != 0) {
+            int min_job_index = 0;
+            if (jobList.get(0)[0] > current_time) {
+                current_time++;
+                continue;
+            }
+            for (int i = 1; i < jobList.size(); i++) {
+                if (jobList.get(i)[0] > current_time) {
+                    break;
+                }
+                if (jobList.get(i)[1] < jobList.get(min_job_index)[1]) {
+                    min_job_index = i;
+                }
+            }
+            answer += (current_time + jobList.get(min_job_index)[1]) - jobList.get(min_job_index)[0];
+            current_time = current_time + jobList.get(min_job_index)[1];
+            jobList.remove(min_job_index);
+        }
+        return answer / jobs.length;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solution(new int[][]{{0,3}, {1,9}, {2,6}}));
     }
 }
